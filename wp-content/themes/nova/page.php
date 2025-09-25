e559c56787dca1e9b97f30000e1ff146e3f2f95b
Jakub Kořínský <Jakub.Korinsky@student.souepl.cz>
2025-09-25 11:26:35 +0200

    a

--- wp-content/themes/nova/page.php
+++ wp-content/themes/nova/page.php
@@ -1,34 +1,43 @@
-<?php
-
-get_header();
-?>
-
-<main id="primary" class="site-main">
+<!DOCTYPE html>
+<html <?php language_attributes();?>>
+    <head>
+        <meta charset="<?php bloginfo( 'charset' );?>">
+        <meta name="viewport" content="width=device-width, initial-scale-1.0">
+        <title>Document</title>
+    </head>
+    <body>
     <?php
-    while (have_posts()) :
-        the_post();
+        get_header();
         ?>
-        <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-            <header class="entry-header">
-                <?php the_title('<h1 class="entry-title">', '</h1>'); ?>
-            </header>
 
-            <div class="entry-content">
-                <?php
-                the_content();
-
-                wp_link_pages(array(
-                    'before' => '<div class="page-links">',
-                    'after'  => '</div>',
-                ));
+        <main id="primary" class="site-main">
+            <?php
+            while (have_posts()) :
+                the_post();
                 ?>
-            </div>
-        </article>
-    <?php
-    endwhile;
-    ?>
-</main>
+                <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+                    <header class="entry-header">
+                        <?php the_title('<h1 class="entry-title">', '</h1>'); ?>
+                    </header>
 
-<?php
-get_footer();
-?>
+                    <div class="entry-content">
+                        <?php
+                        the_content();
+
+                        wp_link_pages(array(
+                            'before' => '<div class="page-links">',
+                            'after'  => '</div>',
+                        ));
+                        ?>
+                    </div>
+                </article>
+            <?php
+            endwhile;
+            ?>
+        </main>
+
+        <?php
+        get_footer();
+        ?>
+    </body>
+</html>
