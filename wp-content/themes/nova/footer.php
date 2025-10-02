d9e58ef8bd442ed1df43db7bcbfdc095db4b4450
Jakub Kořínský <Jakub.Korinsky@student.souepl.cz>
2025-10-02 13:05:11 +0200

    oj

---
+++ wp-content/themes/nova/footer.php
@@ -0,0 +1,30 @@
+<footer>
+    <div class="wrapper">
+        <h2>Zde se nachází footer</h2>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Beatae, dolorum qui nobis tempora neque error architecto debitis atque non quibusdam libero et esse dignissimos soluta hic iste sed modi. Cupiditate.</p>
+        <div class="wrapper-content-social">
+            <a href="#">
+
+            </a>
+            <a href="#">
+
+            </a>
+            <a href="#">
+
+            </a>
+            <a href="#">
+
+            </a>
+        </div>
+    </div>
+    <div class="wrapper-footer">
+        <div class="wrapper-footer-left">
+            <p>© 2025 Všechna práva vyhrazena</p>
+        </div>
+        <div class="wrapper-footer-right">
+            <a href="#">Privacy Policy</a>
+            <a href="#">Terms of Service</a>
+            <a href="#">Contact us</a>
+        </div>
+    </div>
+</footer>
